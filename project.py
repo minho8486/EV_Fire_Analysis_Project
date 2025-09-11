ffc792e23a2e3a8723281dcbdee89d6a17965a7a
minho8486 <minho8486@gmail.com>
2025-09-11 10:48:24 +0900

    tab3 수정5

--- project.py
+++ project.py
@@ -397,7 +397,7 @@ with tab3:
         orientation="h", 
         text=ev_region.values,
         textposition='outside',
-        marker_color='viridis',
+        marker_color='royalblue',
         name='지역별 전기차 화재'
     ))
     fig.update_layout(
@@ -418,7 +418,7 @@ with tab3:
         orientation="h",
         text=charger_region.values,
         textposition='outside',
-        marker_color='cividis',
+        marker_color='forestgreen',
         name='지역별 전기차 충전기'
     ))
     fig.update_layout(
